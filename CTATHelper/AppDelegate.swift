12f6b8776ef894a234ca3d8c7df2d71e2fa3a475
Hsing Liu <liu.hsing@gmail.com>
2019-09-27 19:48:37 -0400

    Initial Commit

---
+++ CTATHelper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CTATHelper
+//
+//  Created by Paul on 9/27/19.
+//  Copyright © 2019 Mathemusician.net. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CTATHelper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CTATHelper
+//
+//  Created by Paul on 9/27/19.
+//  Copyright © 2019 Mathemusician.net. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
